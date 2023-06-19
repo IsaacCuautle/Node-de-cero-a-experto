6d58eb77665002e3d1cded1abef3fff69fa57140
Isaac <cuautle.tron@gmail.com>
2023-06-19 13:53:47 -0600

    first commit

---
+++ 02- Fundamentos/const-var-let.js
@@ -0,0 +1,9 @@
+// var crea variables de ambito global
+const nombre = 'wolvarine'
+
+if(true){
+    const nombre = 'magneto';
+    console.log(nombre);
+} 
+ 
+console.log(nombre);
---
+++ 02- Fundamentos/template-string.js
@@ -0,0 +1,14 @@
+const nombre = 'deadpool';
+const real = 'wade winston';
+
+const normal = nombre+ ' es '+ real;
+// las comillas invertidas se activan con alt+96
+const template = `${nombre} es ${real}`;
+
+console.log(normal);
+console.log(template);
+
+const html = `<h1>Hola</h1>
+<p>Mundo</p>`;
+
+console.log(html);
