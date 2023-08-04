35b29d32993e5f5c0e53de40d26d00ddc9f9c36e
Isaac <cuautle.tron@gmail.com>
2023-08-04 12:12:13 -0600

    modified:   04-Tareas-hacer/db/data.json 	modified:   06-Web-server/.gitignore 	modified:   06-Web-server/app.js 	new file:   06-Web-server/package-lock.json 	modified:   06-Web-server/package.json 	new file:   06-Web-server/public/assets/css/font-awesome.min.css 	new file:   06-Web-server/public/assets/css/main.css 	new file:   06-Web-server/public/assets/fonts/FontAwesome.otf 	new file:   06-Web-server/public/assets/fonts/fontawesome-webfont.eot 	new file:   06-Web-server/public/assets/fonts/fontawesome-webfont.svg 	new file:   06-Web-server/public/assets/fonts/fontawesome-webfont.ttf 	new file:   06-Web-server/public/assets/fonts/fontawesome-webfont.woff 	new file:   06-Web-server/public/assets/fonts/fontawesome-webfont.woff2 	new file:   06-Web-server/public/assets/js/jquery.min.js 	new file:   06-Web-server/public/assets/js/jquery.scrollex.min.js 	new file:   06-Web-server/public/assets/js/jquery.scrolly.min.js 	new file:   06-Web-server/public/assets/js/main.js 	new file:   06-Web-server/public/assets/js/skel.min.js 	new file:   06-Web-server/public/assets/js/util.js 	new file:   06-Web-server/public/images/banner.jpg 	new file:   06-Web-server/public/images/banner2.jpg 	new file:   06-Web-server/public/images/banner3.jpg 	new file:   06-Web-server/public/images/banner4.jpg 	new file:   06-Web-server/public/images/banner5.jpg 	new file:   06-Web-server/public/images/pic01.jpg 	new file:   06-Web-server/public/images/pic02.jpg 	new file:   06-Web-server/views/elements.hbs 	new file:   06-Web-server/views/generic.hbs 	new file:   06-Web-server/views/home.hbs 	new file:   06-Web-server/views/partials/footer.hbs 	new file:   06-Web-server/views/partials/header.hbs 	new file:   06-Web-server/views/partials/navbar.hbs

--- 06-Web-server/app.js
+++ 06-Web-server/app.js
@@ -1,12 +1,47 @@
-import http from 'http'; 
+import hbs from 'hbs';
+import express from "express";
+import * as url from 'url';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+const app = express();
+
+// Handlebars
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
+const port = 8080;
+
+// Servir contenido estatico
+app.use(express.static('public'))
 
 
+app.get('/', (req,res) => {
+    res.render('home',{
+        nombre: 'Isaac',
+        titulo: 'Curso de NODE'
+    });
+});
 
-http.createServer((req,res) =>{
-    res.writeHead(200);
-    res.write('HOLA MUNDO')
-    res.end('');
-})
-.listen(9010)
+app.get('/generic', (req,res) => {
+    res.render('generic',{
+        nombre: 'Isaac',
+        titulo: 'Curso de NODE'
+    });
+});
 
-console.log('escuchando en el puerto', 9010);
+app.get('/elements', (req,res) => {
+    res.render('elements',{
+        nombre: 'Isaac',
+        titulo: 'Curso de NODE'
+    });
+});
+
+
+app.get('*', (req,res) => {
+    res.sendFile( __dirname + '/public/404.html');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
